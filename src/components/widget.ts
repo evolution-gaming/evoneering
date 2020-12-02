26aef331d87e32fbd835d7696583bc1ed431cb2a
mgegernieks <mgegernieks@evolutiongaming.com>
2020-12-02 19:03:28 +0200

    INT-33760 Changes Logo, LogoEvolution, widgets.ts

--- src/components/widget.ts
+++ src/components/widget.ts
@@ -25,10 +25,10 @@ export function initAmsterdamWidget(className: string) {
 
 export function initRigaWidget(className: string) {
     (window as any).widget({
-        "company_code": "EvolutionGaming",
+        "company_code": "Evolution",
         "bg_color_widget": "#ffffff",
-        "bg_color_headers": "#A0ABB8",
-        "bg_color_links": "#7ec0ff",
+        "bg_color_headers": "#969696",
+        "bg_color_links": "#55dd89",
         "txt_color_headers": "#292929",
         "txt_color_job": "#3d3d3d",
         "bg_color_even_row": "#e0e0e0",
@@ -37,23 +37,24 @@ export function initRigaWidget(className: string) {
         "auto_height": "auto",
         "number": "on",
         "job_title": "true",
-        "location": "false",
+        "dateFormat": "us",
+        "remove_headers": "true",
         "filter_locations": "Riga",
         "filter_departments": "867058",
         "departments_field_id": "58b7e4f4e4b09a6d37a0cef4",
         "jobAdType": "PUBLIC",
-        "trid": "",
+        "trid": "f07185a3-ccdf-4bc9-a97e-055e0377a811",
         "api_url": "https://www.smartrecruiters.com",
-        "custom_css_url": "https://static.smartrecruiters.com/job-widget/1.4.2/css/smart_widget.css"
+        "custom_css_url": "https://static.smartrecruiters.com/job-widget/1.5.5/css/smart_widget.css"
     }, className)
 }
 
 export function initTallinnWidget(className: string) {
     (window as any).widget({
-        "company_code": "EvolutionGaming",
+        "company_code": "Evolution",
         "bg_color_widget": "#ffffff",
-        "bg_color_headers": "#A0ABB8",
-        "bg_color_links": "#7ec0ff",
+        "bg_color_headers": "#969696",
+        "bg_color_links": "#55dd89",
         "txt_color_headers": "#292929",
         "txt_color_job": "#3d3d3d",
         "bg_color_even_row": "#e0e0e0",
@@ -62,23 +63,24 @@ export function initTallinnWidget(className: string) {
         "auto_height": "auto",
         "number": "on",
         "job_title": "true",
-        "location": "false",
+        "dateFormat": "us",
+        "remove_headers": "true",
         "filter_locations": "Tallinn",
         "filter_departments": "867058",
         "departments_field_id": "58b7e4f4e4b09a6d37a0cef4",
         "jobAdType": "PUBLIC",
-        "trid": "",
+        "trid": "f07185a3-ccdf-4bc9-a97e-055e0377a811",
         "api_url": "https://www.smartrecruiters.com",
-        "custom_css_url": "https://static.smartrecruiters.com/job-widget/1.4.2/css/smart_widget.css"
+        "custom_css_url": "https://static.smartrecruiters.com/job-widget/1.5.5/css/smart_widget.css"
     }, className)
 }
 
 export function initMinskWidget(className: string) {
     (window as any).widget({
-        "company_code": "EvolutionGaming",
+        "company_code": "Evolution",
         "bg_color_widget": "#ffffff",
-        "bg_color_headers": "#A0ABB8",
-        "bg_color_links": "#7ec0ff",
+        "bg_color_headers": "#969696",
+        "bg_color_links": "#55dd89",
         "txt_color_headers": "#292929",
         "txt_color_job": "#3d3d3d",
         "bg_color_even_row": "#e0e0e0",
@@ -87,13 +89,14 @@ export function initMinskWidget(className: string) {
         "auto_height": "auto",
         "number": "on",
         "job_title": "true",
-        "location": "false",
+        "dateFormat": "us",
+        "remove_headers": "true",
         "filter_locations": "Minsk",
         "filter_departments": "867058",
         "departments_field_id": "58b7e4f4e4b09a6d37a0cef4",
         "jobAdType": "PUBLIC",
-        "trid": "",
+        "trid": "f07185a3-ccdf-4bc9-a97e-055e0377a811",
         "api_url": "https://www.smartrecruiters.com",
-        "custom_css_url": "https://static.smartrecruiters.com/job-widget/1.4.2/css/smart_widget.css"
+        "custom_css_url": "https://static.smartrecruiters.com/job-widget/1.5.5/css/smart_widget.css"
     }, className)
 }
